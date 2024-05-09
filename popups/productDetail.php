34e1a7ba8d1ecfff05fec87a5227b5ecc127b1bb
deniseS85 <denisabel78@hotmail.de>
2024-05-09 12:00:16 +0200

    add close button in productdetail and add separator

--- popups/productDetail.php
+++ popups/productDetail.php
@@ -1,10 +1,14 @@
 <div id="productDetailPopup" class="new-category-popup-bg" onclick="togglePopup('productDetailPopup')" data-product-id="">
     <div class="product-detail-container" onclick="doNotClose(event)">
         <h4 class="popup-title">Produktdetails</h4>
+        <div class="separator"></div>
         <div class="icon-container-edit-product">
             <img onclick="togglePopupDeleteProduct(this)" src="./assets/img/delete.png" class="delete-icon">
             <img src="./assets/img/edit.png" class="edit-icon">
         </div>
-        <div id="productDetailContent"></div> 
+        <div id="productDetailContent"></div>
+        <div class="btn-container-product-detail">
+            <button onclick="togglePopup('productDetailPopup')">Schließen</button>
+        </div>
     </div>
 </div>
